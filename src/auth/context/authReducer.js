80b87a7a935e483b86541e6c0ea08cec5a9cd8e1
AndresDGo <andresdaniel.go2020@gmail.com>
2024-03-24 23:13:46 -0500

    Adding

--- src/auth/context/authReducer.js
+++ src/auth/context/authReducer.js
@@ -1,15 +1,13 @@
 import { types } from "../types/types";
 
 
-
-
 export const authReducer = (state = initialState,action) =>{
     switch (action.type) {
         case types.login:
             return {
                 ...state,
                 logged:true,
-                name:action.payload
+                user:action.payload
             };
 
         case types.logout:
